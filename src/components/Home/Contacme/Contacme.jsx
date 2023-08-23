04d8321bd21da9289b2914a2a9cc15e5f3d56850
IMartinez Isaza <isaacsenatecnica123@gmail.com>
2023-08-22 20:37:56 -0500

    disable capchat and fix something resonsive

--- src/components/Home/Contacme/Contacme.jsx
+++ src/components/Home/Contacme/Contacme.jsx
@@ -6,10 +6,15 @@ const Contacme = () => {
         <section id='contacme'>
         <div className="form_container">
             <h1>Contactame</h1>
-            <form action="https://formsubmit.co/isaacsenatecnica123@gmail.com" method="POST">      
+            <form  action="https://formsubmit.co/isaacsenatecnica123@gmail.com" method="POST">      
                 <input name="name" type="text" class="feedback-input" placeholder="Nombre" />   
                 <input name="email" type="text" class="feedback-input" placeholder="Correo: isaacsenatecnica123@gmail.com" />
                 <textarea name="text" class="feedback-input" placeholder="Ingresa Tu Mensaje"></textarea>
+                
+                <input type="hidden" name='_captcha' value='false'/>
+                <input type="hidden" name='_subject' value='Nuevo Mensaje De Contacto'/>
+                <input type="hidden" name="_template" value="table" />
+
                 <input type="submit" value="ENVIAR"/>
             </form>
         </div>
